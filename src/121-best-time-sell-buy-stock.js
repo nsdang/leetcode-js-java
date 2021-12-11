73fcbfe87a1a1ba817eaa2028fc5927126ec5b5f
Dang Nguyen <dangns229@gmail.com>
2021-12-10 16:38:30 -0800

    121

---
+++ src/121-best-time-sell-buy-stock.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    let min = prices[0];
+    let maxProfit = 0;
+    for (let i = 1; i < prices.length; i++){
+        if (min > prices[i])    min = prices[i];
+        else    maxProfit = Math.max(maxProfit,prices[i] - min);
+                console.log(prices[i]+ "-" + min + "=" +maxProfit);
+    }
+    return maxProfit;
+};
